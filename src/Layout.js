a586f0024c4aff3a82234358c6d847fdfd8fe3d3
Dun2901 <lamchidung373@gmail.com>
2024-05-30 22:17:14 +0700

    add login and route

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 const Layout = () => {
   return (
@@ -8,6 +9,7 @@ const Layout = () => {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </>
