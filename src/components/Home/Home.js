85c841bb748a338bfdeb621f2fb63f8c5c99cb67
haoduylang <21130347@st.hcmuaf.edu.vn>
2024-07-07 21:15:31 +0700

    hao them Footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { IoHome } from "react-icons/io5";
+import { SiMinutemailer } from "react-icons/si";
+import { GiRotaryPhone } from "react-icons/gi";
 
 const Home = () => {
   return (
@@ -7,7 +11,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png.jpg"
+            src="./src/theme.png"
             alt="Card"
             height={500}
           />
@@ -25,6 +29,71 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <footer className="text-center text-lg-start bg-body-tertiary text-muted">
+        <section class=" justify-content-center justify-content-lg-between p-4 border-bottom "  >
+        </section>
+        <section class="justify-content-center justify-content-lg-between p-4 border-bottom"  style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+          <div class="container text-md-start mt-5">
+          <div class="row mt-3">
+          <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-3 text-md-start fst-italic">
+              <i class="fas fa-gem me-3"></i>Gullveig
+            </h6>
+            <p>
+              Choose us for the best quality products.
+            </p>
+          </div>
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+              OUR COMPANY
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">CARRERS</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">CREADIT</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">CORPORATE SOCIAL RESPONSIBILITY</a>
+            </p>         
+          </div>
+     
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+            LEGAL AREA
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">TERMS OF USE</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">PRIVACY POLICY</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">ACCESSIBILITY STATEMENT</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">HUMAN RIGHTS STATEMENT </a>
+            </p>
+          </div>
+  
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+            CONTACT US
+            </h6>
+            <p><IoHome/><i class="">Bien Hoa, Dong Nai</i></p>
+            <p><SiMinutemailer/><i class="">gullveig@fashion.mail</i></p>
+            <p><GiRotaryPhone/><i class=""></i> + 852 162 351</p>
+            </div>
+        </div>
+
+      </div>
+    </section>
+    <div className="text-center p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+      © 2024 Copyright:
+      <a class="text-reset fw-bold" href="#!"> GULLVEIG.com</a>
+    </div>
+
+    </footer>
     </>
   );
 };
