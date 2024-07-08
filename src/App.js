252070bece6aebbb9e48aa03de46156729875075
Dun2901 <lamchidung373@gmail.com>
2024-07-08 21:09:48 +0700

    test api

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 
-function App() {
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3080/api/products") // thay đổi URL phù hợp với server của bạn
+      .then((response) => {
+        console.log("Response:", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Data:", data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Có lỗi xảy ra khi lấy dữ liệu:", error);
+      });
+  }, []);
+
   return (
-    <div className="app-container">
-      <Navbar />
-      <div className="main-container">
-        <div className="app-content">
-          <Outlet />
-        </div>
-      </div>
+    <div>
+      <h1>Danh sách Items</h1>
+      <ul>
+        {/* {data.map((item) => (
+          <li key={item.id}>{item.name}</li> // Giả sử item có thuộc tính id và name
+        ))} */}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
