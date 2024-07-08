9621f9f18d692ac211d74cb2b470df2e2e107a29
haoduylang <21130347@st.hcmuaf.edu.vn>
2024-07-08 13:04:33 +0700

    chuyen tu giaodien sang interface

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,41 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavLink } from "react-router-dom";
+import 'react-multi-carousel/lib/styles.css';
+
 
 const Home = () => {
+
   return (
     <>
       <div className="hero border-1 pb-3">
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png.jpg"
+            src="./assets/theme.png"
             alt="Card"
             height={500}
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">
-                New Season Arrivals
+              <h5 className="card-title fs-1 text fw-lighter fw-bold">
+              NEW ARRIVALS
               </h5>
               <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Winter is co'ming. Get ready with our new collection.
               </p>
+              <div>
+                <NavLink className="nav-link" to="/product">
+                  <button type="button" class="btn btn-outline-light fw-bold">SHOP NOW</button>
+                </NavLink>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+  
+  
     </>
   );
 };
