ebf72d8ffb878c20d3e0407e06d19592449c9006
Dun2901 <lamchidung373@gmail.com>
2024-07-10 14:39:22 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,36 +3,18 @@ import "./style/App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 
-import React, { useEffect, useState } from "react";
-
-const App = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3080/api/products") // thay đổi URL phù hợp với server của bạn
-      .then((response) => {
-        console.log("Response:", response);
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Data:", data);
-        setData(data);
-      })
-      .catch((error) => {
-        console.error("Có lỗi xảy ra khi lấy dữ liệu:", error);
-      });
-  }, []);
-
+function App() {
   return (
     <div className="app-container">
       <Navbar />
       <div className="main-container">
         <div className="app-content">
           <Outlet />
+          <Footer />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,56 +2,50 @@ import { useEffect, useState } from "react";
 import ProductItems from "./ProductItems";
 import React from "react";
 
-
 function Product() {
-    const [products, setProducts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(2);
-    const productsPerPage = 12;
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(2);
+  const productsPerPage = 12;
 
-    useEffect(() => {
-        fetch("http://localhost:3080/api/products")
-        .then((response) => response.json()).then(data=>{
-            setProducts(data);
-            console.log(data);
-        }
-        );
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3080/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        console.log(data);
+      });
+  }, []);
 
-    const indexOfLastProduct = currentPage * productsPerPage;
-    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
-    let pageNumb;
-    const paginate = (pageNumber) => { 
-        setCurrentPage(pageNumber);
-        pageNumb = pageNumber;
+  let pageNumb;
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    pageNumb = pageNumber;
+  };
 
-     };
-
-    return (
-        <div className="App">
-            <h1 className="thisIsHOne">Product</h1>
-            <div className="product-grid">
-                {
-                    currentProducts.map(product => (
-                        <li key={product.id} className="product-item" href="!=">
-                            <img src={product.image} alt={product.name} />
-                            <h2 className="fw-lighter fs-7">{product.name}</h2>
-                            <p className="fw-bold fs-8">${product.price}</p>
-                           
-                        </li>
-        
-                    ))
-                }
-            </div>
-            <ProductItems
-                productsPerPage={productsPerPage}
-                totalProducts={products.length}
-                paginate={paginate}
-                currentPage={currentPage}
-                num={pageNumb}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1 className="thisIsHOne">Product</h1>
+      <div className="product-grid">
+        {currentProducts.map((product) => (
+          <li key={product.id} className="product-item" href="!=">
+            <img src={product.image_url} alt={product.name} />
+            <h2 className="fw-lighter fs-7">{product.name}</h2>
+            <p className="fw-bold fs-8">${product.price}</p>
+          </li>
+        ))}
+      </div>
+      <ProductItems
+        productsPerPage={productsPerPage}
+        totalProducts={products.length}
+        paginate={paginate}
+        currentPage={currentPage}
+        num={pageNumb}
+      />
+    </div>
+  );
 }
 export default Product;
