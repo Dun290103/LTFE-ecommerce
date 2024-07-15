99144589f9e018dc78ce7255d7f104a98a0badd8
Dun2901 <lamchidung373@gmail.com>
2024-07-15 17:25:46 +0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import Layout from "./Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CartProvider } from "./components/Cart/CartContext";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <ToastContainer />
-        <Layout />
-      </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <CartProvider>
+          <ToastContainer />
+          <Layout />
+        </CartProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
 );
---
+++ src/redux/reducer/cartReducer.js
@@ -0,0 +1,7 @@
+import { CART_ADDITEM, CART_DELITEM } from "../action/cartAction";
+
+const INITTIAL_STATE = [];
+
+const cartReducer = (state = INITTIAL_STATE, action) => {};
+
+export default cartReducer;
