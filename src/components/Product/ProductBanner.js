ebe578f4803608784ee274fac213c3009cf37a07
Dun2901 <lamchidung373@gmail.com>
2024-08-03 19:37:23 +0700

    fix UI sort and add pagination-select

--- src/components/Product/ProductBanner.js
+++ src/components/Product/ProductBanner.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
+import { GoTriangleDown } from "react-icons/go";
 
 const ProductBanner = (props) => {
   const { currentSort, onChange } = props;
@@ -10,17 +10,16 @@ const ProductBanner = (props) => {
   };
 
   return (
-    <div>
+    <div className="controls-container">
       {/* =========================================================
                             Left Part Start here
         ======================================================== */}
 
-      <div>
+      <div className="sort-options">
         <Tabs
           id="controlled-tab-example"
           activeKey={currentSort}
           onSelect={(k) => handleSortChange(k)}
-          className="mb-3"
         >
           <Tab eventKey="asc" title="Giá thấp tới cao"></Tab>
           <Tab eventKey="desc" title="Giá cao tới thấp"></Tab>
@@ -32,7 +31,22 @@ const ProductBanner = (props) => {
       {/* =========================================================
                             Right Part STart here
         ======================================================== */}
-      <div>Right Part</div>
+
+      <div className="pagination-select-container">
+        <label className="pagination-label">Show:</label>
+        <select className="pagination-select">
+          <option value="12">12</option>
+          <option value="24">24</option>
+          <option value="36">36</option>
+          <option value="48">48</option>
+        </select>
+        <span className="pagination-dropdown-icon">
+          <GoTriangleDown />
+        </span>
+      </div>
+      {/* =========================================================
+                            Right Part End here
+        ======================================================== */}
     </div>
   );
 };
